baa79233da7ae1db4689e32144deb53cff2dc2b2
TutorialsAndroid <akshaysunilmasram@yahoo.com>
2019-06-09 22:12:34 +0530

    Migrated To AndroidX

---
+++ library/build/generated/source/buildConfig/androidTest/debug/com/kinda/alert/test/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.kinda.alert.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.kinda.alert.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 10;
+  public static final String VERSION_NAME = "2.0.19";
+}
