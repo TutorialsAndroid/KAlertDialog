521f17597c27ad813e055e776ef04a308587705c
TutorialsAndroid <akshaysunilmasram@yahoo.com>
2022-10-18 15:37:00 +0530

    New version v20.2.3

--- sample/build/generated/source/buildConfig/debug/com/developer/kalert/alert/BuildConfig.java
+++ sample/build/generated/source/buildConfig/debug/com/developer/kalert/alert/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "com.developer.kalert.alert";
   public static final String BUILD_TYPE = "debug";
-  public static final int VERSION_CODE = 27;
-  public static final String VERSION_NAME = "20.2.0";
+  public static final int VERSION_CODE = 29;
+  public static final String VERSION_NAME = "20.2.2";
 }
